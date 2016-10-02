5e68b8ad834d371717b7eaa6724fe1a78f80f965
Aidan Breen <aido179@users.noreply.github.com>
2016-10-02 22:26:59 +0100

    Added descriptive comments

--- popup.js
+++ popup.js
@@ -1,7 +1,12 @@
+/*
+Popup.js
+This file is run when the popup.html file is rendered.
+ie. When the user clicks on the extension icon.
+*/
+//convenienve function to update the UI
 function message(val){
   $(".message").text(val);
 }
-
 
 $(document).ready(function(){
     
